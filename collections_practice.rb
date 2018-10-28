43770eaa28808e04a695efa094d21d161f500779
Sung Bae <singsang2@gmail.com>
2018-10-28 01:16:28 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,2 +1,38 @@
 def sort_array_asc(array)
+  new_array = array.sort {|x, y| x<=>y}
+end
+
+def sort_array_desc(array)
+  new_array = array.sort {|x, y| y<=>x}
+end
+
+def sort_array_char_count(array)
+  new_array = array.sort_by {|x| x.length}
+end
+
+def swap_elements(array, i=1, j=2)
+  array[i], array[j] = array[j], array[i]
+end
+
+def reverse_array(array)
   
+end
+
+def kesha_maker(array)
+  array.map! do |word|
+    word[2] = "$"
+  end
+end
+
+def find_a(array)
+  new_array = []
+  array.each {|word| new_array << word if word.start_with?("a")}
+end
+
+def sum_array(array)
+  array.inject {|memo, i| memo += i}
+end
+
+def add_s(array)
+  array.each_with_index.collect {|element, index| element+="s" if index != 1}
+end
